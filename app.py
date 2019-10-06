e285782675d079a78918c2f8fddd6651c04990ce
Damilola Adekoya <dharmykoya38@gmail.com>
2019-10-06 22:33:32 +0100

    chore(setup-database):to setup database To install flask-migrate To create database To create tables To add new columns

--- app.py
+++ app.py
@@ -5,57 +5,34 @@
 import json
 import dateutil.parser
 import babel
+from models import db
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+import sys
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
-from forms import *
+# from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+
+
+db.init_app(app)
+
 
 # TODO: connect to a local postgresql database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres@localhost:5432/fyyur'
 
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
+migrate = Migrate(app, db)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+moment = Moment(app)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
 # Filters.
